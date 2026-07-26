1f300532bf4a48acde5a920d30f905be18e6ddc0
PK-06 <pierrick.samson369@gmail.com>
2026-07-26 15:05:05 +0200

    littles changes

--- app.py
+++ app.py
@@ -291,4 +291,9 @@ def catalog_stats():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=443,
+        ssl_context=("ssl/cert.pem", "ssl/key.pem"),
+        debug=True
+    )
